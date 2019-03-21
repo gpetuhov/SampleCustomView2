87176d524f013cbc52371194937c2d4568cd9e2a
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-21 18:44:58 +0300

    Add onDraw

--- app/src/main/java/com/gpetuhov/android/samplecustomview2/MyCustomView.kt
+++ app/src/main/java/com/gpetuhov/android/samplecustomview2/MyCustomView.kt
@@ -1,10 +1,16 @@
 package com.gpetuhov.android.samplecustomview2
 
 import android.content.Context
+import android.graphics.Canvas
 import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.RequiresApi
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.graphics.Rect
+
 
 class MyCustomView : View {
 
@@ -12,13 +18,38 @@ class MyCustomView : View {
     // only secondary constructors, that call the corresponding
     // constructors of the super class.
 
-    constructor(context: Context) : super(context)
+    constructor(context: Context) : super(context) {
+        init(null)
+    }
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(attrs)
+    }
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(attrs)
+    }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) :
-            super(context, attrs, defStyleAttr, defStyleRes)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        // TODO: implement
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        val paint = Paint(ANTI_ALIAS_FLAG)
+        paint.color = Color.MAGENTA
+        val rect = Rect()
+        rect.left = 0
+        rect.right = width
+        rect.top = 0
+        rect.bottom = height
+
+        canvas?.drawRect(rect, paint)
+    }
 }
